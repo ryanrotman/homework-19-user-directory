cf00f047a429a0ef7fb1297c2e4b46e8a624771b
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-06 23:52:50 -0600

    added Filter component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Table from "./components/Table";
+import Filter from "./components/Filter";
 
 function App() {
     return (
         <div>
             <Navbar />
             <br />
+            <Filter />
             <br />
             <Table />
         </div>
