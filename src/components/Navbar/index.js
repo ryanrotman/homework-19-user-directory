fe13260f862fc159149e387fec94f13c48dc8b84
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-08 15:06:52 -0600

    added new necessary props and fields to search form

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ function Navbar(props) {
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="/"><span><i className="fas fa-users"></i></span>&nbsp;&nbsp;Employee Directory</a>
             <form className="form-inline">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={props.employeeSearch} value={props.searchValue} />
                 <button className="btn btn-dark my-2 my-sm-0" type="submit" onSubmit={props.employeeSearch}>Search</button>
             </form>
         </nav>
