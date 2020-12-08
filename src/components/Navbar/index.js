c97822463d2c641195e6d1587bc69c9cd8d7c92b
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-08 11:29:41 -0600

    added font awesome icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
         <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="/">Employee Directory</a>
+            <a className="navbar-brand" href="/"><span><i class="fas fa-users"></i></span>&nbsp;&nbsp;Employee Directory</a>
             <form className="form-inline">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
