493486d1f2b5c8ffea640e2c8cf7408e60168ff7
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-08 14:13:47 -0600

    changed search button class to btn-dark

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ function Navbar(props) {
             <a className="navbar-brand" href="/"><span><i className="fas fa-users"></i></span>&nbsp;&nbsp;Employee Directory</a>
             <form className="form-inline">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onSubmit={props.employeeSearch}>Search</button>
+                <button className="btn btn-dark my-2 my-sm-0" type="submit" onSubmit={props.employeeSearch}>Search</button>
             </form>
         </nav>
     );
