1839dba200bd98547351de59716404788983d91a
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-08 14:09:58 -0600

    added props for employeeSearch

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,10 @@
-function Navbar() {
+function Navbar(props) {
     return (
         <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="/"><span><i class="fas fa-users"></i></span>&nbsp;&nbsp;Employee Directory</a>
+            <a className="navbar-brand" href="/"><span><i className="fas fa-users"></i></span>&nbsp;&nbsp;Employee Directory</a>
             <form className="form-inline">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onSubmit={props.employeeSearch}>Search</button>
             </form>
         </nav>
     );
