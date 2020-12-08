0ca2eba01838f61c07abcb91af4f882e270921d9
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-08 15:58:20 -0600

    added table-responsive class to div surrounding the table for mobile responsiveness

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,30 +1,32 @@
 function Table(props) {
 
     return (
-        <table className="table table-striped">
-            <thead className="thead-dark">
-                <tr>
-                    <th scope="col"></th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">State</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.list.map(employee => {
-                    return (
-                        <tr key={employee.id.value}>
-                            <td className="align-middle"><img src={employee.picture.medium} alt={employee.name.first + " " + employee.name.last} /></td>
-                            <td className="align-middle">{employee.name.first + " " + employee.name.last}</td>
-                            <td className="align-middle">{employee.phone}</td>
-                            <td className="align-middle">{employee.email}</td>
-                            <td className="align-middle">{employee.location.state}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table className="table table-striped">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">State</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.list.map(employee => {
+                        return (
+                            <tr key={employee.id.value}>
+                                <td className="align-middle"><img src={employee.picture.medium} alt={employee.name.first + " " + employee.name.last} /></td>
+                                <td className="align-middle">{employee.name.first + " " + employee.name.last}</td>
+                                <td className="align-middle">{employee.phone}</td>
+                                <td className="align-middle">{employee.email}</td>
+                                <td className="align-middle">{employee.location.state}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
